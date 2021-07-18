57fba0c2df479e54046444e8f084abc1d5823bfb
Michael Appleby <michael.appleby@me.com>
2021-07-17 21:05:57 -0400

    Focus on open manifests, if any.

--- src/CanvasMap.js
+++ src/CanvasMap.js
@@ -2,37 +2,61 @@ import {MapContainer, TileLayer, GeoJSON, Tooltip, useMap} from 'react-leaflet';
 import React, {useEffect, useState} from 'react';
 import { bbox, featureCollection, bboxPolygon} from '@turf/turf';
 
+
+
 export default function CanvasMap(props) {
+
+
     console.log(props.manifests);
     console.log(props.windows);
     const map = useMap();
     const [geojsonComponents, setGeojsonComponents] = useState([]);
     const manifests = props.manifests;
+    const windows = props.windows;
+
 
     useEffect(() => {
         if (Object.values(manifests).length > 0) {
-            let geojson = Object.values(manifests)
-                .filter(m => !m.isFetching)
+            const allManifests = Object.values(manifests).filter(m => !m.isFetching);
+            let geojson = allManifests
                 .filter(m => m.json.navPlace)
                 .map(m => m.json.navPlace);
             console.log(geojson);
+
+            // What should the map focus on?
+            let zoomGeoJson = geojson;
+            let windowIds = [];
+            if (Object.values(windows).length > 0) {
+                windowIds = Object.values(windows).map(w => w.manifestId);
+            }
+            if (windowIds.length > 0) {
+                let openManifests = allManifests.filter(m => windowIds.includes(m.id));
+                zoomGeoJson = openManifests
+                    .filter(m => m.json.navPlace)
+                    .map(m => m.json.navPlace);
+            }
+
             if (geojson.length > 0) {
+                // Draw all on map
                 const newGeojsonComponents = geojson.map((data, index) =>
                     <GeoJSON
                         key={data.id}
                         data={data}
                     ><Tooltip>Location {data.features[0].properties.label.en}</Tooltip></GeoJSON>);
                 console.log(newGeojsonComponents);
-                const features = geojson.map(json => bboxPolygon(bbox(json)));
+
+                // Get region of interest and zoom map
+                const features = zoomGeoJson.map(json => bboxPolygon(bbox(json)));
                 console.log(features);
                 const allBounds = bbox(featureCollection(features));
                 console.log(allBounds);
                 map.fitBounds([[allBounds[1], allBounds[0]], [allBounds[3], allBounds[2]]]);
+
                 props.setNeedsUpdate(true);
                 setGeojsonComponents(newGeojsonComponents);
             }
         }
-    }, [props.manifests]);
+    }, [props.manifests, props.windows]);
 
 
    return (
