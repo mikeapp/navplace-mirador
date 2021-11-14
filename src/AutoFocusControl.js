1ef560610388757dcda6c871d41c58e98cfa68e9
Michael Appleby <michael.appleby@me.com>
2021-11-14 16:07:11 -0500

    Remove unneeded manifest function/style

--- src/AutoFocusControl.js
+++ src/AutoFocusControl.js
@@ -19,18 +19,6 @@ export default function AutoFocusControl(props) {
         padding: "0.5em"
     }
 
-    const buttonStyle = {
-        backgroundColor: "#00DD00",
-        padding: "1em"
-    }
-
-    const addManifest = (e, uri) => {
-        e.preventDefault();
-        props.addManifest(uri);
-    }
-
-    const prefix = window.location.origin ;
-
     return (
         <div className={positionClass}>
             <div className="leaflet-control leaflet-bar" style={style}>
