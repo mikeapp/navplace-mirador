a331f070945e83fb28ea5b35d24986814862438e
Michael Appleby <michael.appleby@me.com>
2021-11-14 16:08:58 -0500

    Initial work for red markers

--- src/CanvasMap.js
+++ src/CanvasMap.js
@@ -1,6 +1,8 @@
-import {TileLayer, GeoJSON, Tooltip, useMap} from 'react-leaflet';
+import {TileLayer, GeoJSON, Tooltip, Marker, useMap} from 'react-leaflet';
+import L from 'leaflet';
 import React, {useEffect, useState} from 'react';
 import { bbox, featureCollection, bboxPolygon} from '@turf/turf';
+import {icon} from "leaflet/dist/leaflet-src.esm";
 
 export default function CanvasMap(props) {
     console.log(props.manifests);
@@ -9,6 +11,25 @@ export default function CanvasMap(props) {
     const [geojsonComponents, setGeojsonComponents] = useState([]);
     const manifests = props.manifests;
     const windows = props.windows;
+    const iconProps = {
+        iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon-2x.png',
+        shadowUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    }
+    const blueIcon = L.icon(iconProps);
+    const redIcon = L.icon({
+        ...iconProps,
+        className: "redIcon"
+    });
+    const blueMarker = (geoJsonPoint, latlng) => {
+        return new L.Marker(latlng, {icon: blueIcon});
+    }
+    const redMarker = (geoJsonPoint, latlng) => {
+        return new L.Marker(latlng, {icon: redIcon});
+    }
 
     const addNewWindow = (uri) => {
         let manifestIds = Object.values(windows).map(w => w.manifestId);
@@ -38,8 +59,9 @@ export default function CanvasMap(props) {
             if (Object.values(windows).length > 0) {
                 windowIds = Object.values(windows).map(w => w.manifestId);
             }
+            let openManifests = [];
             if (windowIds.length > 0) {
-                let openManifests = allManifests.filter(m => windowIds.includes(m.id));
+                openManifests = geojsonManifests.filter(m => windowIds.includes(m.id));
                 zoomGeoJson = openManifests
                     .filter(m => m.json.navPlace)
                     .map(m => m.json.navPlace);
@@ -47,37 +69,41 @@ export default function CanvasMap(props) {
 
             if (geojson.length > 0) {
                 // Draw all on map
-                const newGeojsonComponents = geojson.map((data, index) =>
-                    <GeoJSON
+                const openManifestIds = openManifests.map(m => m.id);
+                const newGeojsonComponents = geojson.map((data, index) => {
+                    const currentManifestId = geojsonManifests[index].id;
+                    const highlight = openManifestIds.includes(currentManifestId);
+                    console.log("Highlight: " + highlight);
+                    const pointMarker = (highlight)? redMarker : blueMarker;
+                    return (<GeoJSON
                         key={data.id}
                         data={data}
                         eventHandlers={{
                             click: () => addNewWindow(geojsonManifests[index].id)
                         }}
+                        pointToLayer={pointMarker}
                     >
                         <Tooltip>{getLabel(geojsonManifests[index].json)}</Tooltip>
-                    </GeoJSON>);
-                console.log(newGeojsonComponents);
+                    </GeoJSON>)
+                });
 
                 // Get region of interest and zoom map
                 if (props.zoom) {
                     if (zoomGeoJson.length > 0) {
                         const features = zoomGeoJson.map(json => bboxPolygon(bbox(json)));
-                        console.log(features);
+                        // console.log(features);
                         const allBounds = bbox(featureCollection(features));
-                        console.log(allBounds);
+                        // console.log(allBounds);
                         map.fitBounds([[allBounds[1], allBounds[0]], [allBounds[3], allBounds[2]]]);
                     } else {
                         map.fitWorld();
                     }
                 }
-
-                props.setNeedsUpdate(true);
                 setGeojsonComponents(newGeojsonComponents);
+                props.setNeedsUpdate(true);
             }
         }
     }, [props.manifests, props.windows]);
-
 
    return (
        <>
